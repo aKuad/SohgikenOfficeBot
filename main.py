ee5f4d83153ca80136e639c21aac29d6ab1a2668
aKuad <groover_kei_0918@i.softbank.jp>
2020-08-27 00:12:45 +0900

    Install slash command 'echo'

--- main.py
+++ main.py
@@ -4,7 +4,12 @@
 
 from flask import Flask, request
 import os
+
+import scmd.echo
+
 app = Flask(__name__)
+TOKEN_BEARER = os.environ['S_TOKEN_BEARER']
+TOKEN_VERIFY = os.environ['S_TOKEN_VERIFY']
 
 @app.route("/hello", methods=['POST'])
 def wh_hello():
@@ -12,6 +17,24 @@ def wh_hello():
   print (request.headers)
   print ("body: %s" % request.get_data())
   return '''{"ok": true, "message": "Hello from server"}'''
+
+@app.route("/scmd/echo", methods=['POST'])
+def wh_scmd_echo():
+  print ("---- Slash command 'echo' has run ----")
+  print (request.headers)
+  print ("body: %s" % request.get_data())
+
+  if TOKEN_VERIFY != request.form.get("token"):
+    return '''{"ok": false, "message": "Invalid token"}''', 401
+
+  datas = {
+    "channel_id": request.form.get("channel_id"),
+    "user_id": request.form.get("user_id"),
+    "user_name": request.form.get("user_name"),
+    "text": request.form.get("text")
+  }
+  print (scmd.echo(TOKEN, datas))
+  return ''
 
 @app.errorhandler(404)
 def er_404(e):
---
+++ scmd/echo.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+#
+# echo.py
+
+import requests
+
+def echo(TOKEN, datas):
+  post_url = "https://slack.com/api/chat.postMessage"
+  post_head = {"Content-type": "application/json; charset=UTF-8;", "Authorization": "Bearer " + TOKEN}
+  post_body = {
+    "channel": datas["channel_id"],
+    "as_user": True,
+    "text": "",
+    "attachments": [
+      {
+        "color": "good",
+        "text": datas["text"],
+        "footer": "by <@" + datas["user_id"] + "|" + datas["user_name"] + ">"
+      }
+    ]
+  }
+
+  result = requests.post(post_url, headers=post_head, json=post_body)
+
+  return result.text
