7053e1deac1cf5179ffa55e45dd8846a4056bce2
aKuad <groover_kei_0918@i.softbank.jp>
2020-09-16 12:43:42 +0900

    'main.py' comment out added

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
+# coding=UTF-8
 #
 # main.py
 #
 
+# Module import
+## Standard & Extension modules
 from flask import Flask, request
 import os
 import sys
 
+## Local modules
 sys.path.append("./scmd")
-
 import echo
 
+
+# App object make and token load
 app = Flask(__name__)
 TOKEN_BEARER = os.environ['S_TOKEN_BEARER']
 TOKEN_VERIFY = os.environ['S_TOKEN_VERIFY']
 
+
+# App route define
+## Server response test
 @app.route("/hello", methods=['POST'])
 def wh_hello():
   print ("---- Item Received ----")
@@ -21,6 +29,7 @@ def wh_hello():
   print ("body: %s" % request.get_data())
   return '''{"ok": true, "message": "Hello from server"}'''
 
+## Slash command '/echo'
 @app.route("/scmd/echo", methods=['POST'])
 def wh_scmd_echo():
   print ("---- Slash command 'echo' has run ----")
@@ -39,10 +48,13 @@ def wh_scmd_echo():
   echo.echo(TOKEN_BEARER, datas)
   return ''
 
+## 404 error response
 @app.errorhandler(404)
 def er_404(e):
   print ("---- Client error 404 ----")
   return '''{"ok": false, "message": "Invalid path"}''', 404
 
+
+# App run
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=os.environ['PORT'])
