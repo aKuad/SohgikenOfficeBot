3de91d812302b46fc407c6a6398a8e2ee56752f6
aKuad <groover_kei_0918@i.softbank.jp>
2020-09-16 11:30:03 +0900

    Base syntax update and ephemeral option installed

--- scmd/echo.py
+++ scmd/echo.py
@@ -13,31 +13,48 @@ def echo(TOKEN_BEARER, datas):
   # Option check
   ## Variables initialize
   flag_help = False
+  flag_ephe = False
   flag_smpl = False
   post_text = datas["text"]
 
-  if datas["text"] == "":
-    # When empty imput, print help
-    flag_help = True
-  else :
+
+  # Argument check - is argument exist
+  ## When argument is exist
+  if datas["text"] != "":
     # Divide with space and loop
     for stt_txpart in datas["text"].split():
       if stt_txpart == "--help":
         flag_help = True
         post_text = re.sub("^ *--help", "", post_text)
         post_text = re.sub("^ *", "", post_text)
+      elif stt_txpart == "-e" or stt_txpart == "--ephemeral":
+        flag_ephe = True
+        post_text = re.sub("^ *(-e|--ephemeral)", "", post_text)
+        post_text = re.sub("^ *", "", post_text)
       elif stt_txpart == "-s" or stt_txpart == "--simple":
         flag_smpl = True
         post_text = re.sub("^ *(-s|--simple)", "", post_text)
         post_text = re.sub("^ *", "", post_text)
-      else :
+      else:
         break
+  ## When argument is not exist  
+  else:
+    # Print help page
+    flag_help = True
+
+
+  # Argument check - is echo string is exist
+  if post_text == "":
+    # Print help page
+    flag_help = True
+
+
+  # Make post headder
+  post_head = {"Content-type": "application/json; charset=UTF-8;", "Authorization": "Bearer " + TOKEN_BEARER}
 
 
   # Switch help page or not
-  ## Make post headder
-  post_head = {"Content-type": "application/json; charset=UTF-8;", "Authorization": "Bearer " + TOKEN_BEARER}
-
+  ## When help page print flag is true
   if flag_help == True:
     # Help page print
     post_url = "https://slack.com/api/chat.postEphemeral"
@@ -73,50 +90,65 @@ def echo(TOKEN_BEARER, datas):
             },
             {
               "type": "section",
-              "text": { "type": "plain_text", "text": "  `-s --simple` : Echo without ritch decoration", "emoji": True }
+              "text": { "type": "mrkdwn", "text": "`-e` `--ephemeral` : Print in ephemeral message" }
+            },
+            {
+              "type": "section",
+              "text": { "type": "plain_text", "text": "  `-s --simple` : Print without ritch decoration", "emoji": True }
             }
           ],
           "footer": "SohgikenOfficeBot `/echo`"
         }
       ]
     }
-  elif flag_smpl == True:
-    # Echo without ritch decoration
-    ## Make post message
-    post_url = "https://slack.com/api/chat.postMessage"
-    post_body = {
-      "channel": datas["channel_id"],
-      "as_user": True,
-      "text": post_text
-    }
-  else :
-    # Echo with ritch decoration
-    ## Get user icon url
-    get_raw = requests.get("https://slack.com/api/users.info?token=" + TOKEN_BEARER + "&user=" + datas["user_id"])
-    get_dic = json.loads(get_raw.text)
-    post_uico = get_dic["user"]["profile"]["image_72"]
+    stt_result = requests.post(post_url, headers=post_head, json=post_body)
+    print(stt_result.text)
 
-    ## Make post message
-    post_url = "https://slack.com/api/chat.postMessage"
-    post_body = {
-      "channel": datas["channel_id"],
-      "as_user": True,
-      "text": "",
-      "attachments": [
-        {
-          "color": "good",
-          "author_icon": post_uico,
-          "author_name": "<@" + datas["user_id"] + "|" + datas["user_name"] + ">",
-          "text": post_text,
-          "footer": "SohgikenOfficeBot `/echo`"
-        }
-      ]
-    }
+  ## When help page print flag is false
+  else:
+    # Branch with ephemeral flag is true or not
+    post_url = ""
+    if flag_ephe == True:
+      post_url = "https://slack.com/api/chat.postEphemeral"
+    else:
+      post_url = "https://slack.com/api/chat.postMessage"
 
+    # Branch with simple flag is true or not
+    ## When true, print without ritch decoration
+    if flag_smpl == True:
+      # Make & post message
+      post_body = {
+        "channel": datas["channel_id"],
+        "as_user": True,
+        "text": post_text
+      }
+      stt_result = requests.post(post_url, headers=post_head, json=post_body)
+      print(stt_result.text)
+    ## When false, print with ritch decoration
+    else:
+      # Get user icon url
+      get_raw = requests.get("https://slack.com/api/users.info?token=" + TOKEN_BEARER + "&user=" + datas["user_id"])
+      get_dic = json.loads(get_raw.text)
+      post_uico = get_dic["user"]["profile"]["image_72"]
 
-  # Post content
-  result = requests.post(post_url, headers=post_head, json=post_body)
+      # Make & post message
+      post_body = {
+        "channel": datas["channel_id"],
+        "as_user": True,
+        "text": "",
+        "attachments": [
+          {
+            "color": "good",
+            "author_icon": post_uico,
+            "author_name": "<@" + datas["user_id"] + "|" + datas["user_name"] + ">",
+            "text": post_text,
+            "footer": "SohgikenOfficeBot `/echo`"
+          }
+        ]
+      }
+      stt_result = requests.post(post_url, headers=post_head, json=post_body)
+      print(stt_result.text)
 
 
   # Quit
-  return result.text
+  return
