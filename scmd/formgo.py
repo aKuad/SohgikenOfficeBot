f6a3d870330d36d45acaffbbe2571f294b6832bd
aKuad <groover_kei_0918@i.softbank.jp>
2020-09-20 22:49:38 +0900

    block_id and action_id defined

--- scmd/formgo.py
+++ scmd/formgo.py
@@ -24,13 +24,15 @@ def formgo(TOKEN_BEARER, datas):
       "blocks": [
         {
           "type": "input",
+          "block_id": "formgo_make_label",
           "label": { "type": "plain_text", "text": u":label: フォームラベル", "emoji": True },
           "element": {
+            "action_id": "formgo_make_label",
             "type": "plain_text_input",
             "multiline": False,
             "placeholder": { "type": "plain_text", "text": u"フォームラベルを入力" }
           },
-          "hint": { "type": "plain_text", "text": u"このテキストは、複数のフォームの判別に用いられます", "emoji": True},
+          "hint": { "type": "plain_text", "text": u"このテキストは、複数のフォームの判別に用いられます", "emoji": True },
           "optional": False
         },
         {
@@ -38,8 +40,10 @@ def formgo(TOKEN_BEARER, datas):
         },
         {
           "type": "input",
+          "block_id": "formgo_make_type",
           "label": { "type": "plain_text", "text": u":bar_chart: フォームタイプ", "emoji": True },
           "element": {
+            "action_id": "formgo_make_type",
             "type": "radio_buttons",
             "options": [
               {
@@ -55,12 +59,10 @@ def formgo(TOKEN_BEARER, datas):
         },
         {
           "type": "input",
-          "label": {
-            "type": "plain_text",
-            "text": u":busts_in_silhouette: フォーム回答対象者",
-            "emoji": True
-          },
+          "block_id": "formgo_make_member",
+          "label": { "type": "plain_text", "text": u":busts_in_silhouette: フォーム回答対象者", "emoji": True },
           "element": {
+            "action_id": "formgo_make_member",
             "type": "multi_users_select",
             "placeholder": { "type": "plain_text", "text": u"フォーム回答の対象となるメンバー", "emoji": True }
           },
@@ -72,8 +74,10 @@ def formgo(TOKEN_BEARER, datas):
         },
         {
           "type": "input",
+          "block_id": "formgo_make_desc",
           "label": { "type": "plain_text", "text": u":page_facing_up: フォーム説明", "emoji": True },
           "element": {
+            "action_id": "formgo_make_desc",
             "type": "plain_text_input",
             "multiline": True,
             "placeholder": { "type": "plain_text", "text": u"フォーム説明を入力", "emoji": True}
