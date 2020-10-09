c868bbf23c3e0ffb86d46126e3625f85522ca48f
aKuad <groover_kei_0918@i.softbank.jp>
2020-10-10 01:17:40 +0900

    Key reading fixed in 'formgo_bpsh'

--- sint/formgo_bpsh.py
+++ sint/formgo_bpsh.py
@@ -9,10 +9,10 @@ import requests
 
 # Function define
 def formgo_bpsh(TOKEN_BEARER, datas):
-  if datas["actions"][0]["value"] == "answer":
+  if datas["actions"][0]["action_id"] == "answer":
     print("## answer pushed ##")
     pass
-  elif datas["actions"][0]["value"] == "delete":
+  elif datas["actions"][0]["action_id"] == "delete":
     print("## delete pushed ##")
     pass
 
