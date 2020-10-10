906e4ae2da641c88ffce88113c87b11466d2d365
aKuad <groover_kei_0918@i.softbank.jp>
2020-10-10 17:07:56 +0900

    Form body fixed 'trigger_id', 'view', 'callback_id'

--- sint/formgo_bpsh.py
+++ sint/formgo_bpsh.py
@@ -16,59 +16,63 @@ def formgo_bpsh(TOKEN_BEARER, datas):
     post_url = "https://slack.com/api/views.open"
     post_head = {"Content-type": "application/json; charset=UTF-8;", "Authorization": "Bearer " + TOKEN_BEARER}
     post_body = {
-      "title": { "type": "plain_text", "text": u"健康状態調査フォーム", "emoji": True },
-      "type": "modal",
-      "submit": { "type": "plain_text", "text": u":incoming_envelope: 送信",	"emoji": True },
-      "close": { "type": "plain_text",  "text": u":x: キャンセル", "emoji": True },
-      "blocks": [
-        {
-          "type": "input",
-          "label": { "type": "plain_text", "text": u":thermometer: 体温", "emoji": True },
-          "element": {
-          "type": "plain_text_input",
-          "multiline": False,
-          "placeholder": { "type": "plain_text", "text": u"体温を入力", "emoji": True }
-        },
-          "optional": False
-        },
-        {
-          "type": "divider"
-        },
-        {
-          "type": "input",
-          "label": { "type": "plain_text", "text": u":heart: 体調", "emoji": True },
-          "element": {
-            "type": "radio_buttons",
-            "options": [
-              {
-                "text": { "type": "plain_text", "text": u":partyparrot: 絶好調", "emoji": True },
-                "value": "value-0"
-              },
-              {
-                "text": { "type": "plain_text", "text": u":yoshi: 良好", "emoji": True },
-                "value": "value-1"
-              },
-              {
-                "text": { "type": "plain_text", "text": u":face_with_thermometer: 不調", "emoji": True },
-                "value": "value-2"
-              }
-            ]
-          },
-          "optional": False
-        },
-        {
-          "type": "divider"
-        },
-        {
-          "type": "input",
-          "label": { "type": "plain_text", "text": u":pencil: 備考", "emoji": True },
-          "element": {
+      "trigger_id": datas["trigger_id"],
+      "view": {
+        "callback_id": "sint_formgo_sbmt",
+        "title": { "type": "plain_text", "text": u"健康状態調査フォーム", "emoji": True },
+        "type": "modal",
+        "submit": { "type": "plain_text", "text": u":incoming_envelope: 送信",	"emoji": True },
+        "close": { "type": "plain_text",  "text": u":x: キャンセル", "emoji": True },
+        "blocks": [
+          {
+            "type": "input",
+            "label": { "type": "plain_text", "text": u":thermometer: 体温", "emoji": True },
+            "element": {
             "type": "plain_text_input",
-            "multiline": True
+            "multiline": False,
+            "placeholder": { "type": "plain_text", "text": u"体温を入力", "emoji": True }
           },
-          "optional": True
-        }
-      ]
+            "optional": False
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "input",
+            "label": { "type": "plain_text", "text": u":heart: 体調", "emoji": True },
+            "element": {
+              "type": "radio_buttons",
+              "options": [
+                {
+                  "text": { "type": "plain_text", "text": u":partyparrot: 絶好調", "emoji": True },
+                  "value": "value-0"
+                },
+                {
+                  "text": { "type": "plain_text", "text": u":yoshi: 良好", "emoji": True },
+                  "value": "value-1"
+                },
+                {
+                  "text": { "type": "plain_text", "text": u":face_with_thermometer: 不調", "emoji": True },
+                  "value": "value-2"
+                }
+              ]
+            },
+            "optional": False
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "input",
+            "label": { "type": "plain_text", "text": u":pencil: 備考", "emoji": True },
+            "element": {
+              "type": "plain_text_input",
+              "multiline": True
+            },
+            "optional": True
+          }
+        ]
+      }
     }
     stt_result = requests.post(post_url, headers=post_head, json=post_body)
     print(stt_result.text)
